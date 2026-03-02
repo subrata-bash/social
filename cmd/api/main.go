971b7abdbbef5fe47167c51240ee7d2a797672c7
Om Sri Guru Joy <sgj@Oms-Mac-mini.local>
2026-03-02 21:22:13 +0530

    Implementing the repository patern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/subrata-bash/social/internal/env"
+	"github.com/subrata-bash/social/internal/store"
+)
 
 func main() {
 	cfg := config{
-		Addr: ":8080",
+		Addr: env.GetString("ADDR", ":8080"),
 	}
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
+
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
